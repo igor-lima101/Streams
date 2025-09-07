fb8e37e2b3b0d68d6343d081a8abdff305b14a80
igor-lima101 <igormaksym96@gmail.com>
2025-09-07 17:14:20 -0300

    feat: create all streams functions

--- src/StudentManager.java
+++ src/StudentManager.java
@@ -1,5 +1,8 @@
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class StudentManager {
 
@@ -9,5 +12,38 @@ public class StudentManager {
         list.add(student);
     }
 
+    public List<Student> getApprovedStudents(){
+        return list.stream()
+                .filter(student ->  student.getGrade() > 6)
+                .collect(Collectors.toList());
+    }
+
+    public List<String> getNamesUppercase(){
+        return list.stream()
+                .map(student -> student.getName().toUpperCase())
+                .collect(Collectors.toList());
+    }
+
+    public double getAverageGrade(){
+        return list.stream()
+                .mapToDouble(Student::getGrade)
+                .average()
+                .orElse(0.0);
+    }
+
+    public Optional<Student> getWorstStudent() {
+        return list.stream()
+                .min(Comparator.comparingDouble(Student::getGrade));
+    }
+
+    public List<Student> getStudentsSortedByGrade() {
+        return list.stream()
+                .sorted(Comparator.comparingDouble(Student::getGrade))
+                .collect(Collectors.toList());
+    }
+
+    public void showAll() {
+        list.forEach(System.out::println);
+    }
 
 }
